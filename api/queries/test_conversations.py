931e1e1cbf7fe95a5be8118485e27e4f444432db
Andrew Casey <andrew.r.casey@protonmail.com>
2023-08-30 04:15:38 +0000

    Write boilerplate for test_conversations

---
+++ api/queries/test_conversations.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+class TestConversationQueries(TestCase):
+    def test_create(self):
+        # Arrange
+
+        # Act
+
+        # Asset
+        pass
+
+    def test_get_one_by_token(self):
+        # Arrange
+
+        # Act
+        
+        # Assert
+        pass
+
+    def test_get_one_by_id(self):
+        # Arrange
+
+        # Act
+        
+        # Assert
+        pass
+
+    def test_get_all(self):
+        # Arrange
+
+        # Act
+        
+        # Assert
+        pass
+
+    def test_update_one_tokens(self):
+        # Arrange
+
+        # Act
+        
+        # Assert
+        pass
+
+    def test_delete_by_id(self):
+        # Arrange
+
+        # Act
+        
+        # Assert
+        pass
---
+++ api/routers/test_messages.py
@@ -0,0 +1,2 @@
+# The following url gives guidance on how to get started with endpoint tests
+# https://learn-2.galvanize.com/cohorts/3723/blocks/1906/content_files/build/04-unit-tests/04-unit-tests.md
